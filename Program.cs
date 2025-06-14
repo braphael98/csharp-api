740b3569c8f0fc49ed0ec58ca75eed0cb03421be
Bruno Perez <braphaelperez98@gmail.com>
2025-06-14 15:35:35 -0300

    Musicas ok

--- Program.cs
+++ Program.cs
@@ -1,11 +1,15 @@
-﻿using (HttpClient client = new HttpClient())
+﻿using System.Text.Json;
+using csharp_projeto03.Modelos; 
+
+using (HttpClient client = new HttpClient())
 {
-    try//Caminho feliz.
+    try
     {
         string resposta = await client.GetStringAsync("https://guilhermeonrails.github.io/api-csharp-songs/songs.json"); //puxando os dados
-        System.Console.WriteLine(resposta);
+        var musicas = JsonSerializer.Deserialize<List<Musica>>(resposta); //padrao para receber os dados como um objeto.
+        musicas[0].ExibirDetalhesDaMusica();
     }
-    catch (Exception ex)//Deu ruim.
+    catch (Exception ex)
     {
         Console.WriteLine($"PROBLEMA IDENTIFICADO ->> {ex.Message}"); //configurando um possivel erro.
     }
