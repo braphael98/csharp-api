600dfd3886d2b158ae3dac6ceced9fc9662053fa
Bruno Perez <braphaelperez98@gmail.com>
2025-06-17 15:12:12 -0300

    Classe musicas favoritas

---
+++ Modelos/MusicasPreferidas.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_projeto03.Modelos;
+
+internal class MusicasPreferidas
+{
+    public string? Nome { get; set; }
+    public List<Musica> ListaMusicasFavoritas;
+
+    public MusicasPreferidas(string nome)
+    {
+        Nome = nome;
+        ListaMusicasFavoritas = new List<Musica>();
+    }
+    public void AddMusicasFavoritas(Musica musica)
+    {
+        ListaMusicasFavoritas.Add(musica);
+    }
+    public void ExibirMusicasFavoritas()
+    {
+        Console.WriteLine($"Musicas Favoritas de {Nome}");
+        foreach (var musica in ListaMusicasFavoritas)
+        {
+            Console.WriteLine($"{musica.Nome} de {musica.Artista}");
+        }
+    }
+}
+
